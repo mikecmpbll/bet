e7d5b9a4c3b8da3045e8bcdbd82ab758c1a35444
Mike Campbell <mike@wordofmike.net>
2015-07-01 21:24:02 +0100

    Uh, the maths for dutching was completely broken. FIXED!

--- lib/bet/staking.rb
+++ lib/bet/staking.rb
@@ -2,7 +2,7 @@ module Bet
   module Staking
     class << self
       def dutch(prices, risk = 1)
-        denom = prices.combination(2).map{ |c| c[0] * c[1] }.reduce(:+)
+        denom = prices.combination(prices.size - 1).reduce(0){ |m,c| m + c.reduce(:*) }
 
         stakes = prices.map.with_index do |p, i|
           (prices.dup.tap{ |a| a.delete_at(i) }.reduce(:*) * risk)
@@ -10,7 +10,7 @@ module Bet
             .round(2)
         end
 
-        profits     = prices.zip(stakes).map{ |c| c.reduce(:*) }
+        profits     = prices.zip(stakes).map{ |c| c[0] * c[1] }
         total_stake = stakes.reduce(:+)
         min_profit  = profits.min - total_stake
 
